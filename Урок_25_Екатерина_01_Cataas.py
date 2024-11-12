0611ac8f0684850940a56f9d33b8cefc44be33d4
Programmer <shakirov.ekb@gmail.com>
2024-11-12 19:15:00 +0500

    Добавлено меню, удалена кнопка загрузки.

--- Урок_25_Екатерина_01_Cataas.py
+++ Урок_25_Екатерина_01_Cataas.py
@@ -3,8 +3,10 @@ import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
+from Scripts.Урок_18_Екатерина_01_Меню_плюс_картинки import mainmenu
 
-def load_image(url):
+
+def load_image():
     response = requests.get(url)
     print(response)
 #    print(response.headers)
@@ -12,25 +14,42 @@ def load_image(url):
     image_data=BytesIO(response.content)
     print(image_data)
     img=Image.open(image_data)
+    img.thumbnail((500,500)) #Подгоняем картинку под размер
     print(img)
-    return ImageTk.PhotoImage(img)
+    img=ImageTk.PhotoImage(img)
+    label.image = img #Сохраняет нашу картинку
+    label.config(image=img)
+#    return ImageTk.PhotoImage(img)
+    print(label.image)
 
 
 window=Tk()
-window.geometry("500x500")
+window.geometry("500x550")
 
 
-label=Label()
+label=Label(width=500,height=500)
 label.pack()
 
 url="https://cataas.com/cat"
-img=load_image(url)
 
-if img:
-    label.config(image=img)
+#img=load_image(url)
+
+#if img:
+#    label.config(image=img)
 #    label.image=img
 
-but=Button(text="Загрузка картинки",command=load_image)
-but.pack()
+# but=Button(text="Загрузка картинки",command=load_image)
+# but.pack()
+
+mainmenu=Menu(window)
+window.config(menu=mainmenu)
+
+file_menu=Menu(mainmenu,tearoff=0)
+mainmenu.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузка изображения", command=load_image)
+file_menu.add_command(label="Выход", command=window.destroy)
+
+
+load_image()
 
 window.mainloop()
