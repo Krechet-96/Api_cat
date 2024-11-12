ee8c228fdcbe21e8890babf83c3f165e4eaaf6c6
Programmer <shakirov.ekb@gmail.com>
2024-11-12 18:55:48 +0500

    Определена функция для отправки запроса и преобразования ответа в нужный формат.

--- Урок_25_Екатерина_01_Cataas.py
+++ Урок_25_Екатерина_01_Cataas.py
@@ -1,14 +1,19 @@
-from cProfile import label
 from tkinter import *
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
-from bottle import response
-
 
 def load_image(url):
-    pass
+    response = requests.get(url)
+    print(response)
+#    print(response.headers)
+#    print(response.content)
+    image_data=BytesIO(response.content)
+    print(image_data)
+    img=Image.open(image_data)
+    print(img)
+    return ImageTk.PhotoImage(img)
 
 
 window=Tk()
@@ -24,5 +29,8 @@ img=load_image(url)
 if img:
     label.config(image=img)
 #    label.image=img
+
+but=Button(text="Загрузка картинки",command=load_image)
+but.pack()
 
 window.mainloop()
